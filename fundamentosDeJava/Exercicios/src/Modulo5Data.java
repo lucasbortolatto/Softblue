0e26c5a7fd760bb700cc87576ee63f54ba2e6000
Luidson Lucas <lucasbortolatto2@hotmail.com>
2016-10-04 22:55:07 -0300

    Cap05 e 06

--- fundamentosDeJava/Exercicios/src/Modulo5Data.java
+++ fundamentosDeJava/Exercicios/src/Modulo5Data.java
@@ -4,11 +4,11 @@ public class Modulo5Data {
 	private int dia;
 	private int mes;
 	private int ano;
-	private int hora;
-	private int minuto;
-	private int segundo;
-	private final String FORMATO_12H;
-	private final String FORMATO_24H; 
+	private int hora = -1;
+	private int minuto = -1;
+	private int segundo = -1;
+	public static final int FORMATO_12H = 1;
+	public static final int FORMATO_24H = 2; 
 	
 
 	public Modulo5Data(int dia, int mes, int ano){
@@ -21,10 +21,33 @@ public class Modulo5Data {
 	public Modulo5Data(int dia, int mes, int ano, int hora, int minuto, int segundo){
 		this(dia, mes, ano);
 		this.hora = hora;
-		if (hora < 0 || hora > 24)
-			System.out.println("Hora invalida");
 		this.minuto = minuto;
-		this.segundo = segundo;
+		this.segundo = segundo;	
+	}
+	
+	public void imprimir(int formato){
+		String data = dia + "/" + mes + "/" + ano;
+		
+		if (hora == -1){
+			System.out.println(data);
+		}else{
+			String horario = ":" + minuto + ":" + segundo;
+			
+			if (formato == FORMATO_24H){
+				horario = hora + horario;
+			}else{
+				if (hora >= 12){
+					horario =  (hora - 12) + horario;
+					horario += " PM";
+				}else{
+					horario = hora + horario;
+					horario += " AM";
+				}
+			}
+		
+			System.out.println(data + " " +  horario);
+		
+		}
 		
 	}
 
@@ -74,10 +97,6 @@ public class Modulo5Data {
 
 	public void setSegundo(int segundo) {
 		this.segundo = segundo;
-	}
-	
-	public int imprimir(){
-		return forma
 	}
 	
 	
---
+++ fundamentosDeJava/Exercicios/src/Modulo6Figura.java
@@ -0,0 +1,20 @@
+
+public class Modulo6Figura {
+	
+	public class Modulo6Retangulo extends Modulo6Figura{
+		
+	}
+	
+	public class Modulo6Quadrado extends Modulo6Figura{
+		
+	}
+	
+	public class Modulo6FiguraComplexa extends Modulo6Figura{
+		
+	}
+	
+	public double calcularArea(){
+		
+	}
+
+}
